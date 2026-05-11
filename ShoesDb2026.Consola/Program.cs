f4def45af92b6ac90813a1adc3509664929e6e6f
Matias Flores <matiasflores@outlook.com>
2026-05-11 16:16:11 -0300

    Agregar archivos de proyecto.

---
+++ ShoesDb2026.Consola/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ShoesDb2026.Consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ ShoesDb2026.Data/ShoesDb2026DbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoesDb2026.Data
+{
+    public class ShoesDb2026DbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=ShoesDb2026; Trusted_Connection=true; TrustServerCertificate=true;");
+        }
+    }
+}
