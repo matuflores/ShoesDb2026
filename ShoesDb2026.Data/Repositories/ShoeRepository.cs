5927895840d2f8d5fa23276773406253d6378015
Matias Flores <matiasflores@outlook.com>
2026-05-11 19:28:34 -0300

    InterfacesRepositoryAndService

---
+++ ShoesDb2026.Data/Repositories/ShoeRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoesDb2026.Data.Repositories
+{
+    public class ShoeRepository
+    {
+    }
+}
---
+++ ShoesDb2026.Service/Services/SportService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoesDb2026.Service.Services
+{
+    public class SportService
+    {
+    }
+}
